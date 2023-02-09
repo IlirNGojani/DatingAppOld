ebf3df5d1eb67d0cd19ae27b4060e75ee20a963a
Ilir Gojani <ilirngojani@gmail.com>
2023-02-09 15:23:36 +0100

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -7,9 +7,11 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => { opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
+builder.Services.AddCors();
 
 WebApplication app = builder.Build();
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
