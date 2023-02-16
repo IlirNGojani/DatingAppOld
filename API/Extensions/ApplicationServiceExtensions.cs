9950e519955d6582580535996c2f34220ccf9e7c
Ilir Gojani <ilirngojani@gmail.com>
2023-02-16 16:25:41 +0100

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -12,6 +12,8 @@ namespace API.Extensions
             services.AddDbContext<DataContext>(opt => { opt.UseSqlServer(config.GetConnectionString("DefaultConnection")); });
             services.AddCors();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             return services;
         }
---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int CalcuateAge(this DateTime dob)
+        {
+            DateTime today = DateTime.Now;
+            int age = today.Year - dob.Year;
+
+            if (dob > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+}
